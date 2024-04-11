c54b8a3d7ff43debd12a8a30c7a62a79cf9a2c9f
itssiem <siem@siemgerritsen.nl>
2024-04-11 12:18:18 +0200

    Movement improvements

--- src/player.cpp
+++ src/player.cpp
@@ -25,14 +25,18 @@ glm::mat4 player::viewMatrix() {
 
 void player::processInput(const char key) {
     glm::vec3 norm_heading = glm::normalize(heading);
+    glm::vec3 movement_vector;
+    float movement_step = 0.1f;
     if (key == 'w')
-        position += glm::vec3(norm_heading.x, 0, norm_heading.z);
+        movement_vector = glm::vec3(norm_heading.x, 0, norm_heading.z);
     if (key == 's')
-        position += -glm::vec3(norm_heading.x, 0, norm_heading.z);
+        movement_vector = -glm::vec3(norm_heading.x, 0, norm_heading.z);
     if (key == 'a')
-        position += glm::vec3(norm_heading.z, 0, -norm_heading.x);
+        movement_vector = glm::vec3(norm_heading.z, 0, -norm_heading.x);
     if (key == 'd')
-        position += glm::vec3(-norm_heading.z, 0, norm_heading.x);
+        movement_vector = glm::vec3(-norm_heading.z, 0, norm_heading.x);
+
+    position += glm::normalize(movement_vector) * movement_step;
 
     float rotate_step = DegToRad(1);
     glm::mat4 rotation_matrix(1);
